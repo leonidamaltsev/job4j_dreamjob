36a576bc396d61c467acbbab3cf29f2c7ecb4b80
leonidamaltsev <daf.dof@yandex.ru>
2024-11-17 23:37:23 +0300

    4. Многопользовательские приложения. Многопоточность в Веб приложении. [#504857]

---
+++ src/main/java/ru/job4j/dreamjob/controller/CounterController.java
@@ -0,0 +1,18 @@
+package ru.job4j.dreamjob.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RestController
+public class CounterController {
+
+    private final AtomicInteger total = new AtomicInteger(0);
+
+    @GetMapping("/count")
+    public String count() {
+        int value = total.incrementAndGet();
+        return String.format("Total execute : %d", value);
+    }
+}
